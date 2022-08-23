8adcebfc3131b650470085e4366a328872c52eb0
schenna217 <90796839+schenna217@users.noreply.github.com>
2022-08-23 13:12:41 -0500

    Changed theme, added answer box

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.firstEnteredNum);
+        EditText number2ET = findViewById(R.id.secondEnteredNum);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        int sum = num1 + num2;
+
+        numberSumTV.setText("" + sum);
+    }
 
 }
